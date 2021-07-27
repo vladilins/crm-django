b2a2f00087a65eca244b52fcf0524a26940e671e
Vlad <ilin96inbox@gmail.com>
2021-07-27 15:22:21 +0900

    add templates

--- leads/views.py
+++ leads/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('Hello')
+    # return HttpResponse('Hello')
+    return render(request, 'leads/home_page.html')
