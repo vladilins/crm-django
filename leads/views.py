9255002a1662110a3e298ac079bc2e067f62e1af
Vlad <ilin96inbox@gmail.com>
2021-07-31 09:13:17 +0900

    context

--- leads/views.py
+++ leads/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 def home_page(request):
     # return HttpResponse('Hello')
-    return render(request, 'leads/home_page.html')
+    context = {
+        'name': 'John',
+    }
+    return render(request, 'second_page.html', context)
